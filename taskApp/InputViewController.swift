b9979542e421e23a6a056cac17218a04b436a125
Sora-bs <aozoma@icloud.com>
2019-07-17 17:15:56 +0900

    pod install と .gitignoreの作成

--- taskApp/InputViewController.swift
+++ taskApp/InputViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class InputViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
